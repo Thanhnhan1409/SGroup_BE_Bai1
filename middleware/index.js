ee2b893050a20847a01eb506680f2aa9dee670e4
Thanhnhan1409 <phanthithanhnhan1409@gmail.com>
2023-04-07 21:02:35 +0700

    fixed

--- middleware/index.js
+++ middleware/index.js
@@ -6,8 +6,10 @@ function validate(req, res, next){
     if(age <= 0)
         return res.status(400).json("Age must be greater than 0!");
     const regexC = /[^a-zA-ZÀ-ỹ\s]/.test(name);
-    const regexN = /\d/.test(name);
-    if(regexC && regexN)
+   
+    // const regexS = /@|\d|\./.test(name);
+      
+    if(regexC )
         return res.status(400).json("Name cannot contain numbers or special characters!");
     next()
 
